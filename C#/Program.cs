d5913d7027f41dc3ad9ba94dc8df2651213400f9
vijayraj <bsvijayraj@gmail.com>
2023-01-17 11:03:17 +0530

    inital commit

---
+++ C#/Program.cs
@@ -0,0 +1,6 @@
+﻿class helloWorld {
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to C# programming");
+    }
+}
---
+++ C#/ifElse.cs
@@ -0,0 +1,14 @@
+public class controlStatement
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter a num: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if(num % 2 == 0){
+            Console.WriteLine("even num");
+        } else {
+            Console.WriteLine("odd num");
+        }
+    }
+}
